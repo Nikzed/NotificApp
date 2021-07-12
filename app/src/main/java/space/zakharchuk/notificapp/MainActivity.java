88f72160d5e8921eae6b9afd80c473c27fc04955
zakharchuk_nv <nik.shadkov@mail.ru>
2021-07-12 12:14:02 +0300

    work commit

--- app/src/main/java/space/zakharchuk/notificapp/MainActivity.java
+++ app/src/main/java/space/zakharchuk/notificapp/MainActivity.java
@@ -12,6 +12,8 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import static androidx.core.app.NotificationCompat.PRIORITY_DEFAULT;
 
@@ -26,11 +28,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // скрываем строку состояния
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_main);
 
-        // скрываем статус бар
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         TextView mainValue = findViewById(R.id.mainValue);
         mainValue.setText(String.valueOf(count));
